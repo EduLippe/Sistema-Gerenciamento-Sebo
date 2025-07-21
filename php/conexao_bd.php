b897c1910eee14b1ce76429adbee8b877feb5753
EduLippe <edurufino74@gmail.com>
2025-07-21 11:05:27 -0300

    Deploy AWS

--- php/conexao_bd.php
+++ php/conexao_bd.php
@@ -1,13 +1,14 @@
 <?php
-    $host = 'localhost';     // IP do servidor remoto onde está o MySQL
-    $usuario = 'root';             // usuário MySQL remoto
-    $senha = 'Sistema@sebo123.';   // senha correta para esse usuário no servidor remoto
-    $banco = 'bd_soft_livr';       // nome do banco no servidor remoto
+    $host = '15.228.223.139';      // ou 'localhost' se o PHP está na mesma máquina
+    $usuario = 'root';             // ou um usuário específico, se você criou
+    $senha = 'Sistema@sebo123.';
+    $banco = 'bd_srv_livra';
 
     $conexao = new mysqli($host, $usuario, $senha, $banco);
 
     if ($conexao->connect_error) {
         die("Erro na conexão: " . $conexao->connect_error);
     }
-    echo "Conexão bem sucedida ao banco remoto!";
+
+    echo "Conexão bem-sucedida!";
 ?>
